521bc53fb19d851599b3a0bd40fdf2ae2ab2693d
Suganth-DMac <sugaanth.mohan@gmail.com>
2020-10-25 04:24:12 +0530

    Added Code for Partition Based Search

--- Search_Algorithms/Partition_Search.py
+++ Search_Algorithms/Partition_Search.py
@@ -16,10 +16,24 @@ class Partition:
     )
 
     def __init__(self, pidx, container_):
-        self.low = min(container_)
-        self.high = max(container_)
+        self.low = min(set(container_))
+        self.high = max(set(container_))
         self.pidx = pidx
         self.elements = container_
+
+    def __repr__(self):
+        return "Pid: {pid} with range {low} => {high}".format(
+            low=self.low,
+            high=self.high,
+            pid=self.pidx
+        )
+
+    def __str__(self):
+        return "Pid: {pid} with range {low} => {high}".format(
+            low=self.low,
+            high=self.high,
+            pid=self.pidx
+        )
 
 
 class PartitionList:
@@ -36,33 +50,28 @@ class PartitionList:
 
         self.p_count = partitions_count
         self.partitions = np.array(
-            [
+            list(
                 itertools.starmap(
                     self.create_partition,
                     enumerate(np.array_split(container_, partitions_count))
                 )
-            ]
+            )
         )
 
     def search_partition(self, element):
 
-        my_parition = np.array(
+        my_partition = list(
             filter(
-                lambda x: x[1].low <= element <= x[1].high,
+                lambda x: x.low <= element <= x.high,
                 self.partitions
             )
         )
 
-        return {
-            "PN": my_parition[0],
-            "PN_IDX": np.where(my_parition[1] == element)
-        }
+        return my_partition[0].pidx, np.where(my_partition[0].elements, element)
 
 
 if __name__ == '__main__':
     List = np.arange(1000)
     partitions = 10
     my_ds = PartitionList(List, partitions)
-    for item in my_ds.partitions:
-    #print(my_ds.search_partition(30))
-
+    print(my_ds.search_partition(30))
